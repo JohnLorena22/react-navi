064dea52bfcd6391452288bb4911905f15b5aa6b
john <johnbrianlorena79@gmail.com>
2026-08-19 08:27:18 +0800

    react-router-dom

--- my-first-app/src/App.jsx
+++ my-first-app/src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
+
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import './App.css'
 
 import Home from './pages/Home';
